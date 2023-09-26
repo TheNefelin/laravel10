d4cad20ca1e1a1bffa32831c662586aea0ffaa45
Francisco Carmona <flcarmonac@yahoo.com>
2023-09-25 21:45:25 -0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,15 @@
 <x-layouts.app title="Home" meta_description="Meta Home">
 
-  <h1>HOME</h1>
+  <h1>Mapa</h1>
+  <input type="text" name="" id="" placeholder="latitud">
+  <input type="text" name="" id="" placeholder="longitud">
+  <button type="submit">Enviar</button>
+  <iframe 
+    src="https://maps.google.com/maps?q=-33.036,-71.62963&output=embed" 
+    frameborder="0"
+    width="100%"
+    height="500px"
+  ></iframe>
 
 </x-layouts.app>
 
